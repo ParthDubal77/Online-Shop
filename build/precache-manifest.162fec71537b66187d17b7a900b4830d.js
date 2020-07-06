e24f2f9c6ca403570ad2f5636bdf2ad3785daa4a
Parth Dubal <53576241+ParthDubal77@users.noreply.github.com>
2020-07-06 17:04:51 +0530

    Add files via upload

---
+++ build/precache-manifest.162fec71537b66187d17b7a900b4830d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998b06fed17e9b7dc1b88f51dae3d214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ce1451499a5887837f",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "0961390d37f618b93500",
+    "url": "/static/css/main.f0cede6c.chunk.css"
+  },
+  {
+    "revision": "91ce1451499a5887837f",
+    "url": "/static/js/2.dde34cb0.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.dde34cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0961390d37f618b93500",
+    "url": "/static/js/main.51650f7b.chunk.js"
+  },
+  {
+    "revision": "f428cb23cfe510de8ef8",
+    "url": "/static/js/runtime-main.0ae25ea0.js"
+  },
+  {
+    "revision": "d35c7c2f9249e491ebcbcdde42305a24",
+    "url": "/static/media/12.d35c7c2f.jpg"
+  },
+  {
+    "revision": "1684f98e1f8944237b29cd9b69655853",
+    "url": "/static/media/18.1684f98e.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
